63fcc25ff08031c085fd148aabab9f9951f1058c
ceylon85 <canleez123@gmail.com>
2020-06-25 20:15:51 +0900

    Updates

---
+++ precache-manifest.421bfd1e5631e3064d1cb983967d89ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da76832a1af7a51bc25fcae97fa0c78d",
+    "url": "/react-burger-menu/index.html"
+  },
+  {
+    "revision": "331fb12b5cd036b31244",
+    "url": "/react-burger-menu/static/js/2.09193c08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-burger-menu/static/js/2.09193c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17155c9ae557a0b9aebb",
+    "url": "/react-burger-menu/static/js/main.cb316cbb.chunk.js"
+  },
+  {
+    "revision": "6f4d5ac5135c17681b1c",
+    "url": "/react-burger-menu/static/js/runtime-main.7c404fb2.js"
+  }
+]);
